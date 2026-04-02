17f648e9222a891bbc8ad6e269e7f1302833cdb2
mcittkmims <adrian.vremere.swe@gmail.com>
2026-04-02 23:26:40 +0300

    favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const customDomain = process.env.CUSTOM_DOMAIN;
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const owner = process.env.GITHUB_REPOSITORY_OWNER ?? '';
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 const isUserSite = repo.endsWith('.github.io');
 
+const site = customDomain
+  ? `https://${customDomain}`
+  : isGithubActions && owner
+    ? `https://${owner}.github.io`
+    : undefined;
+
+const base = customDomain || !isGithubActions || isUserSite
+  ? '/'
+  : `/${repo}/`;
+
 // https://astro.build/config
-export default defineConfig({
-  site: isGithubActions && repo ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io` : undefined,
-  base: isGithubActions && repo && !isUserSite ? `/${repo}/` : '/',
-});
+export default defineConfig({ site, base });
